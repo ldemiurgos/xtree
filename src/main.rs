1605ef03aae606f98a0500e85c9922c34eea96a0
Luis Demiurgos <luisdemiurgos@gmail.com>
2026-07-03 13:57:06 -0500

    [feat]: List the entries in the current directory

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
-fn main() {
-    println!("Hello, world!");
+use std::{env, fs, io};
+
+fn main() -> io::Result<()> {
+    let path = env::current_dir()?;
+    let entries = fs::read_dir(&path)?;
+    for entry in entries {
+        let entry = entry?;
+        if let Some(entry_name) = entry.path().file_name() {
+            println!("{}", entry_name.display());
+        }
+    }
+    Ok(())
 }
