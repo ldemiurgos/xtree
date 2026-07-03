ca6a43cfde710801c7ee034229109f2d2e80c8fc
Luis Demiurgos <luisdemiurgos@gmail.com>
2026-07-03 14:55:18 -0500

    [feat]: Move Logic from main function to data estructure implementation

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,34 @@
-use std::{env, fs, io};
+use std::{
+    env,
+    fs::{self, DirEntry},
+    io,
+    path::Path,
+};
+
+struct Node {
+    entries: Vec<DirEntry>,
+}
+
+impl Node {
+    fn from<P: AsRef<Path>>(path: P) -> io::Result<Self> {
+        Ok(Self {
+            entries: fs::read_dir(path)?.filter_map(|entry| entry.ok()).collect(),
+        })
+    }
+
+    fn list_names_from_entries(&self) -> Vec<String> {
+        self.entries
+            .iter()
+            .filter_map(|f| f.file_name().into_string().ok())
+            .collect()
+    }
+}
 
 fn main() -> io::Result<()> {
     let path = env::current_dir()?;
-    let entries = fs::read_dir(&path)?;
-    for entry in entries {
-        let entry = entry?;
-        if let Some(entry_name) = entry.path().file_name() {
-            println!("{}", entry_name.display());
-        }
+    let current_node = Node::from(path)?;
+    for filename in current_node.list_names_from_entries() {
+        println!("{filename}");
     }
     Ok(())
 }
